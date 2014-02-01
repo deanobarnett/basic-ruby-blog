c57e5369a15fad7b3c2d3a64c33da5ce441a0827
Dean Barnett <dbarnett419@gmail.com>
2014-02-01 19:32:45 +0000

    blog app with create post edit delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Blog::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
+
+
+  Blog::Application.routes.draw do
+
+    resources :posts
+ 
+    root to: "welcome#index"
+  end
+
+  resources :posts do
+    resources :comments
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
